fa00f0411431b587a64c5fb1558599fe2f6f603c
CallMeAL <alexpgarner@gmail.com>
2023-07-12 20:29:20 -0700

    mobile nav is now vertical and hidden until hamburger clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,14 @@
 		});
 
 })(jQuery);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+	//
+  if (x.className === "container") {
+    x.className += " responsive";
+  } else {
+    x.className = "container";
+  }
+}
